ec4a03dd976f6102a295244ed962c809495894ea
millstone <soyotime0118@gmail.com>
2023-05-16 17:06:20 +0900

    - 동시성테스트 추가

--- home-inventory-management/src/test/java/me/mason/management/application/UseInventoryTest.java
+++ home-inventory-management/src/test/java/me/mason/management/application/UseInventoryTest.java
@@ -60,5 +60,34 @@ class UseInventoryTest {
         });
     }
 
+    @Test
+    public void 동시에_100개의_요청() throws InterruptedException {
+        int threadCount = 100;
+        //멀티스레드 이용 ExecutorService : 비동기를 단순하게 처리할 수 있또록 해주는 java api
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+
+        //다른 스레드에서 수행이 완료될 때 까지 대기할 수 있도록 도와주는 API - 요청이 끝날때 까지 기다림
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        for (int i = 0; i < threadCount; i++) {
+            executorService.submit(() -> {
+                        try {
+                            useInventory.usingInventory(new Inventory.InventoryId(2L), 1);
+                        } finally {
+                            latch.countDown();
+                        }
+                    }
+            );
+        }
+
+        latch.await();
+
+        Inventory inventory = inventoryPersistentRepository.loadByInventoryId(new Inventory.InventoryId(2L))
+                .orElseThrow();
+
+        //100 - (1*100) = 0
+        Assertions.assertEquals(inventory.getQuantity(), 0);
+
+    }
 
 }
